fe5bf3c3b8184e3d8ffdd621471d3138ed75f336
mzheng6 <mzheng6@illinois.edu>
2015-04-22 00:50:40 -0500

    fixed the search.js

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -1,5 +1,5 @@
 'use strict';
-
+ 
 /**
  * @ngdoc function
  * @name 411t2App.controller:SearchCtrl
@@ -10,14 +10,14 @@
 angular.module('411t2App')
   .controller('SearchCtrl', function ($http, $scope) {
     // bind 'this' to vm (view-model)
-	var vm = this;
-
-	var url = 'http://localhost:8000/sql/';
-
+    var vm = this;
+ 
+    var url = 'http://localhost:8000/sql/';
+ 
     $scope.$on('$viewContentLoaded', function() {
       vm.loadAllItems();
     });
-
+ 
     vm.loadAllItems = function(){
         $http
             .get(url + 'select?table=Item&attr=*')
@@ -25,7 +25,13 @@ angular.module('411t2App')
                 vm.sresults = data;
             });
     }
-
+ 
+    vm.loadEnter = function(keyEvent) {
+        if (keyEvent.which === 13){
+            vm.load();
+        }
+    }
+ 
     vm.searchWithQuery = function(){
     /**
     * GET Route /search/item
@@ -43,44 +49,12 @@ angular.module('411t2App')
                 vm.sresults = data;
             });
     }
-
-  	vm.load = function(){
-
-  		/**
-		* GET Route /search/item
-		* @params query
-		* Usage:
-		* GET item/search?query='something to search'
-		*/
-	
-                            $http
-                                .get(url + 'item/search', {
-                                    params: {
-                                        query: vm.squery
-                                    }
-                                })
-                                .success(function(data){
-                                    vm.sresults = data;
-                                    // vm.squery = '';
-                                });
-
-                    // note temp code
-	         /*  $http
-                        .get("http://localhost:8000/auth/profile/items", {
-                            params: {
-                                query: vm.squery
-                            }
-                        })
-                        .success(function(data){
-                            //vm.sresults = data;
-                            // vm.squery = '';
-                        });
-             */
-            };
-  
+ 
+    vm.load = function(){
+ 
         if(vm.squery === '' || vm.squery === undefined) vm.loadAllItems();
         else vm.searchWithQuery();
-
+ 
 /*      // note temp code
         $http
             .get("http://localhost:8000/auth/profile/items", {
@@ -92,13 +66,13 @@ angular.module('411t2App')
                 //vm.sresults = data;
                 // vm.squery = '';
             });*/
-
+ 
     };
-
+ 
   });
-
-
-
+ 
+ 
+ 
 // inventory (items)
 // look up underscore vs hyphen conventions for json
 // look for library/plugin for formatting price
