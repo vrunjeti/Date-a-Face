35bebaa2d5615ecaa47318b5eefe5a5305f8834b
Andy <andyv.42@gmail.com>
2015-04-22 12:46:48 -0500

    Finished routes for user profile

--- backend/router/routes/auth.js
+++ backend/router/routes/auth.js
@@ -1,36 +1,84 @@
 var connection = require('../../connection');
 var express = require('express');
 var jwt = require('jwt-simple');
+var jwt2 = require('jsonwebtokens');
 var settings = require('../../settings');
 
 var router = express.Router();
 
-// Need a profile page that shows all the users items, user's information and has options
-// to mark the item as deleted and delete the user account themselves
-// delete item
-// delete user
-// mark as sold
-
-// fix search
-// notifications to login and additems and addusers
-
-router.get('/test', isAuthenticated, function (req, res, next) {
-    console.log("You're In!");
-    console.log(req.userid);
-    res.json({message: "Success"});
+/** 
+* DELETE /profile/item
+* Params: {itemid} <- needs the itemid so we know which item to delete, this id should be stored with the item on the view.
+* Returns a JSON object.
+*/
+router.delete('/profile/item', isAuthenticated, function (req, res, next) {
+    console.log("Deleting Item!");
+    // we'll take one param to identify the item we're deleting
+    var item_id = req.body.itemid;
+    var q_del = "DELETE FROM Item WHERE id="+ item_id +';';
+    connection.query(q_del, function (err, rows) { 
+        if(err) {
+            res.json({message: "Error Occured"});
+        }
+        else {
+            res.json({message: "Success"});
+        }
+    });
 });
 
+/** 
+* DELETE /profile/all 
+* No params are needed, deletes the whole account.
+* Returns a JSON object. You should probably redirect on success and call localStorage.clear(); to logout the user.
+*/
+router.delete('/profile/all', isAuthenticated, function (req, res, next) {
+    console.log("Deleting Item!");
+    // we'll take one param to identify the item we're deleting
+    var id = req.userid;
+    var q_del = "DELETE FROM User WHERE id="+ id +';';
+    connection.query(q_del, function (err, rows) { 
+        if(err) {
+            res.json({message: "Error Occured"});
+        }
+        else {
+            res.json({message: "Success"});
+        }
+    });
+});
+
+/**
+* GET /profile/logout
+* Returns a JSON object. Should redirect on http request success as well as call localStorage.clear(); before hand.
+*/
+router.get('/profile/logout', isAuthenticated, function (req, res, next) {
+        console.log("Logging out!");
+        res.json({message: "Log Out"})
+});
+
+/**
+* GET /profile
+* Returns the user's info. NOTE: No id param needs to be given because this function gets the id automatically. You just need to call it.
+*/
 router.get('/profile', isAuthenticated, function (req, res, next) {
     console.log("Getting Profile!");
     console.log(req.userid);
     var id = req.userid; // we'll use this to identify our loggedin user
     var q_info = "SELECT * FROM User WHERE  id="+ id +';'; // get all the user's info
     connection.query(q_info, function (err, rows) { 
-        console.log(rows[0]);
-        res.json({message: "Yes"});
+        if(err) {
+            res.json({message: "Error Occured"});
+        }
+        else {
+            console.log(rows[0]);
+            res.json({message: "Success" payload: rows[0]});
+        }
     });
 });
 
+/**
+* GET /profile/items
+* Returns all of the items for a user. NOTE: No id param needs to be given because this function gets the id automatically. You just need to call it.
+*/
 router.get('/profile/items', isAuthenticated, function (req, res, next) {
     console.log("Getting items!");
     console.log(req.userid);
@@ -38,9 +86,21 @@ router.get('/profile/items', isAuthenticated, function (req, res, next) {
     var q_items =  "SELECT * FROM Item INNER JOIN User ON Item.userId=User.id WHERE User.id="+ id +';'; // get all of the user's associated items
     console.log(q_items);
     connection.query(q_items, function (err, rows) { 
-        console.log(rows[0]);
-        res.json({message: "Yes"});
+        if(err) {
+            res.json({message: "Error Occured"});
+        }
+        else {
+            console.log(rows[0]);
+            res.json({message: "Success" payload: rows[0]});
+        }
     });
+});
+
+////// Other stuff //////
+router.get('/test', isAuthenticated, function (req, res, next) {
+    console.log("You're In!");
+    console.log(req.userid);
+    res.json({message: "Success"});
 });
 
 router.post('/signup', function (req, res, next) {
@@ -58,6 +118,7 @@ router.post('/signup', function (req, res, next) {
                 res.json({message: "User Exists"});
             }
             else {
+  
                 var q = 'INSERT INTO User (firstName, lastName, email, password, phone) VALUES ( ?, ?, ?, ?, ? );'; 
                 connection.query(q, [connection.escape(req.body.firstName), connection.escape(req.body.lastName), connection.escape(req.body.email), connection.escape(req.body.password), connection.escape(req.body.phone)] , function (err, rows) {
                     if(err) {
@@ -115,7 +176,6 @@ router.post('/login', function (req, res, next) {
         }
     });
 });
-
 
 module.exports = router;
 
